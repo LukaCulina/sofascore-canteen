f127a903f599257b83cd6f2dd522235de30e0c3f
Nyletic <nikolicmihael1@gmail.com>
2026-03-05 08:45:47 +0100

    added canteen page

--- src/pages/Canteen/Canteen.tsx
+++ src/pages/Canteen/Canteen.tsx
@@ -1,3 +1,6 @@
 export const CanteenPage = () => {
-  return null
+  return (<div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Canteen Stranca</h1>
+    </div>
+  )
 }
