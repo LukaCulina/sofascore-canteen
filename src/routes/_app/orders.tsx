be1562849586aff7a144bb6c201c0eda8f9bcbba
Luka Čulina <luka.culina56@gmail.com>
2026-03-12 23:40:57 +0100

    Poperly implemetned the orders route

--- src/routes/_app/orders.tsx
+++ src/routes/_app/orders.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
 import { Orders } from "@/pages/Orders"
 
-export const Route = createFileRoute("/orders")({
+export const Route = createFileRoute("/_app/orders")({
   component: Orders,
   head: () => ({
     meta: [
