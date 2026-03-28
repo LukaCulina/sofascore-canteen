392edbe5089ca321232d9bf1dc29e69363eb3cdb
Nyletic <nikolicmihael1@gmail.com>
2026-03-28 14:19:56 +0100

    display meal catalog

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { Button } from "./Button"
 export { Input } from "./Input"
+export { LoadingSpinner } from "./LoadingSpinner/LoadingSpinner"
 export { MealCard } from "./MealCard"
 export * from "./Text"
