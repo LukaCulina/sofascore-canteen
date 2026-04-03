f2bd411c2c54e6475cfa6fbd6117585d41e8e8db
Mihael Nikolić <39503541+Nyletic@users.noreply.github.com>
2026-04-03 22:28:49 +0200

    Update src/api/hooks.ts
    Co-authored-by: Tamara Verk <54901082+TamaraVerk@users.noreply.github.com>

--- src/api/hooks.ts
+++ src/api/hooks.ts
@@ -3,7 +3,7 @@ import { getJson } from "@/api/http-client"
 import { useAuthStore } from "@/stores/auth"
 
 export function useAuthSWR<T>(url: string) {
-  const { token } = useAuthStore.getState()
+  const { token } = useAuthStore()
 
   const fetcher = (url: string) => {
     if (!token) {
