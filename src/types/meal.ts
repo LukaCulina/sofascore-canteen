6d4bb08c0ebf0ade76e175f4b2d26a580f1181d0
ImraKocis <imrakocis@gmail.com>
2026-04-10 11:20:00 +0200

    PBSRB-25 | merge conflicts and fixes

--- src/types/meal.ts
+++ src/types/meal.ts
@@ -16,29 +16,3 @@ export interface DayMeal {
 export interface Meals {
   meals: Meal[]
 }
-
-export interface MealApiResponse {
-  meals: {
-    id: number
-    description: string
-    price: number
-    is_vegetarian: boolean
-    discount: number
-    image_url: string
-    average_rating?: number
-    review_count: number
-  }[]
-}
-
-export function transformMeal(apiMeal: MealApiResponse["meals"][0]): Meal {
-  return {
-    id: apiMeal.id,
-    description: apiMeal.description,
-    price: apiMeal.price,
-    isVegetarian: apiMeal.is_vegetarian,
-    discount: apiMeal.discount,
-    imageUrl: apiMeal.image_url,
-    averageRating: apiMeal.average_rating,
-    reviewCount: apiMeal.review_count,
-  }
-}
