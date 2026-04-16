6d6fb6ed1bd167852c4d971dffe2e52f98e6502c
editagelic <edita@coiagency.co>
2026-04-16 19:23:57 +0200

    refactor: remove manual auth header from useAuthSWRMutation

--- src/hooks/useAuthSWRMutation.ts
+++ src/hooks/useAuthSWRMutation.ts
@@ -1,13 +1,6 @@
 import useSWRMutation from "swr/mutation"
-import { postJson } from "@/api/http-client.ts"
-import { useAuthStore } from "@/stores/auth.ts"
+import { postJson } from "@/api/http-client"
 
 export function useAuthSWRMutation<TBody extends object, TResponse>(url: string) {
-  const { token } = useAuthStore()
-
-  return useSWRMutation(url, (url, { arg }: { arg: TBody }) =>
-    postJson<TResponse>(url, arg, {
-      headers: { Authorization: `Bearer ${token}` },
-    }),
-  )
+  return useSWRMutation(url, (url, { arg }: { arg: TBody }) => postJson<TResponse>(url, arg))
 }
