6698e9ec3483829d4e9fa69d438dfb0087f3e797
Luka Čulina <luka.culina56@gmail.com>
2026-05-04 18:28:59 +0200

    feat: add new routes and interface for meal transfer

--- src/types/transfer.ts
+++ src/types/transfer.ts
@@ -2,3 +2,10 @@ export interface Transfer {
   user_id: string
   to_user_display_name: string
 }
+
+export interface User {
+  id: string
+  name: string
+  surname: string
+  displayName: string
+}
