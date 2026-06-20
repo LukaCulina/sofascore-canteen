46810dcfb6085f39327168b34e54ebd88b2e1b9c
Luka Čulina <106603546+LukaCulina@users.noreply.github.com>
2026-06-20 21:44:51 +0200

    Update API base URL for http-client

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -1,5 +1,5 @@
 import { useAuthStore } from "@/stores/auth"
-export const API_BASE_URL = "https://frontend-academy.sofascore-api.deno.net/api"
+export const API_BASE_URL = "https://frontend-academy-api-2026.lukaculina.deno.net/"
 
 type JsonRequestMethod = "POST" | "PUT" | "DELETE"
 
